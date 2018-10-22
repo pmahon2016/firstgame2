3644c61c6f6fa3e48f6a7f0df454bd5650efe945
paulmahon <paulmahon@me.com>
2018-10-22 14:44:08 -0400

    After refactoring successfully

--- drone.py
+++ drone.py
@@ -1,17 +1,35 @@
 import pygame
+import game_settings as settings
+
+
+screen=pygame.display.set_mode((settings.screen_width,settings.screen_height))
 
 image = pygame.image.load("drone.png")
 
 rect = image.get_rect()
+screen_rect = screen.get_rect()
 
-def load_image(screen):
+# rect.centerx = screen_rect.centerx
+rect.right = screen_rect.right
+# rect.bottom = screen_rect.bottom
+rect.top = screen_rect.top
 
-    screen_rect = screen.get_rect()
 
-    # rect.centerx = screen_rect.centerx
-    rect.right = screen_rect.right
-    # rect.bottom = screen_rect.bottom
-    rect.top = screen_rect.top
+
+def blitme():
     direction = 'left'
+    screen.fill(settings.bg_color)
+    if direction == 'right':
+        rect.centerx +=5
+        if rect.centerx > 10:
+            direction = 'left'
 
+    elif direction == 'left':
+        rect.centerx -= 5
+        if rect.left < -50:
+            rect.right = screen_rect.right
+            rect.top = screen_rect.top
+            direction = 'left'
 
+    # 6 - draw the screen elements
+    screen.blit( image, rect)
--- game_controls.py
+++ game_controls.py
@@ -1,7 +1,4 @@
 import pygame
-import drone
-
-
 
 def check_events():
     direction = "left"
@@ -12,16 +9,3 @@ def check_events():
                     pygame.quit()
                     exit(0)
 
-    if direction == 'right':
-        drone.rect.centerx += 5
-        if drone.rect.centerx > 10:
-            direction = 'left'
-
-    elif direction == 'left':
-        drone.rect.centerx -= 5
-        if drone.rect.left < -50:
-            drone.rect.right = drone.screen_rect.right
-            drone.rect.top = drone.screen_rect.top
-            direction = 'left'
-
-    screen.blit(image, rect)
